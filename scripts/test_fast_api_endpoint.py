a0377cd37ebc7e45660ec1b38ae0030a7cce13b5
AshokChoudhary06 <ashokchoudhary66836@gmail.com>
2026-09-19 20:05:42 +0530

    final_commit

--- scripts/test_fast_api_endpoint.py
+++ scripts/test_fast_api_endpoint.py
@@ -13,8 +13,8 @@ def test_predict_endpoint():
         response = client.post(f"/predict" , json = data)
 
     assert response.status_code == 200 , "Api failed to process the prediction"
-    assert isinstance(response.json(),  dict)
 
+    
 def test_generate_cloud():
     data = {
         "comment": ["Love this!", "Not so great.", "Absolutely amazing!", "Horrible experience."]
