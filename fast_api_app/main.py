15685679d1efe5bd74215d8b533bdc92b25f4c49
AshokChoudhary06 <ashokchoudhary66836@gmail.com>
2026-09-19 19:49:43 +0530

    final_code_changes

--- fast_api_app/main.py
+++ fast_api_app/main.py
@@ -29,7 +29,7 @@ async def load_model(app: FastAPI):
 
         mlflow.set_tracking_uri("http://16.171.44.208:8000/")
         model_path = f"models:/yt_chrome_plugin_model/1"
-        app.state.model = mlflow.pyfunc.load_model(model_path)
+        app.state.model = mlflow.sklearn.load_model(model_path)
         app.state.vectorizer = joblib.load(os.path.join(ROOT_DIR,"tfidf_vectorizer.pkl"))
         yield
         print("Shutting the server and clearing memory")
